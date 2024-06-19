a5982360b688513a8400004ded98a7b4018231a7
jihyeon-Do <ehwlgus22@gmail.com>
2024-06-19 16:48:50 +0900

    refactor : move data

---
+++ src/data/temporary.js
@@ -0,0 +1,28 @@
+import recommandThumbnail from "../../public/recommand-plan.jpg";
+
+export const recommandPlans = [
+  {
+    id: 1,
+    thumbnail: recommandThumbnail,
+    title: "국내 당일치기 당진여행",
+    location: "당진",
+  },
+  {
+    id: 2,
+    thumbnail: recommandThumbnail,
+    title: "국내 당일치기 당진여행",
+    location: "당진",
+  },
+  {
+    id: 3,
+    thumbnail: recommandThumbnail,
+    title: "국내 당일치기 당진여행",
+    location: "당진",
+  },
+  {
+    id: 4,
+    thumbnail: recommandThumbnail,
+    title: "국내 당일치기 당진여행",
+    location: "당진",
+  },
+];

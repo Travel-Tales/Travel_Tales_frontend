3b2f5c25606b9aa43473705a26cb97954da75040
jihyeon-Do <62285862+jihyeon-Do@users.noreply.github.com>
2024-06-19 17:16:44 +0900

    Revert "refactor home page"

--- src/app/not-found.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function NotFount() {
-  return <h1>Not Found</h1>;
-}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,33 +7,11 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px",
-    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        "main-banner": "url('../../public/main-banner.jpg')",
-      },
-      backgroundPosition: {
-        "bottom-1": "center bottom -5rem",
-      },
-      fontSize: {
-        h1: ["60px", { lineHeight: "1.2" }],
-        h2: ["48px", { lineHeight: "1.3" }],
-        h3: ["36px", { lineHeight: "1.4" }],
-        h4: ["24px", { lineHeight: "1.5" }],
-        h5: ["20px", { lineHeight: "1.6" }],
-        h6: ["16px", { lineHeight: "1.7" }],
-      },
-      padding: {
-        "c-padding": "0 1.5rem 0 1.5rem",
       },
     },
   },

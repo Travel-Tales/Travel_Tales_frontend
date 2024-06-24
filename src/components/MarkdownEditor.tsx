81f9ff20d2afcbe278126ccde6ce7ba6a4608958
jihyeon-Do <ehwlgus22@gmail.com>
2024-06-25 01:21:36 +0900

    style: complete markdown component

---
+++ src/components/MarkdownEditor.tsx
@@ -0,0 +1,80 @@
+"use client";
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { nord } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+// import "./MarkdownEditor.css";
+
+const MarkdownEditor = ({ markdown }: { markdown: string }) => {
+  return (
+    <div className="w-1/2 p-2 overflow-hidden break-all">
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code({ className, children }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return match ? (
+              // 코드 (```)
+              <SyntaxHighlighter style={nord} language={match[1]} PreTag="div">
+                {String(children)
+                  .replace(/\n$/, "")
+                  .replace(/\n&nbsp;\n/g, "")
+                  .replace(/\n&nbsp\n/g, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <SyntaxHighlighter
+                style={nord}
+                background="green"
+                language="textile"
+                PreTag="div"
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            );
+          },
+          // 인용문 (>)
+          blockquote({ children, ...props }) {
+            return (
+              <blockquote
+                style={{
+                  background: "#7afca19b",
+                  padding: "1px 15px",
+                  borderRadius: "10px",
+                }}
+                {...props}
+              >
+                {children}
+              </blockquote>
+            );
+          },
+          img({ ...props }) {
+            return (
+              <img
+                style={{ maxWidth: "40vw" }}
+                src={props.src?.replace("../../../../public/", "/")}
+                alt="MarkdownRenderer__Image"
+              />
+            );
+          },
+          em({ children, ...props }) {
+            return (
+              <span style={{ fontStyle: "italic" }} {...props}>
+                {children}
+              </span>
+            );
+          },
+        }}
+      >
+        {markdown
+          .replace(/\n/gi, "\n\n")
+          .replace(/\*\*/gi, "@$_%!^")
+          .replace(/@\$_%!\^/gi, "**")
+          .replace(/<\/?u>/gi, "*")}
+      </ReactMarkdown>
+    </div>
+  );
+};
+
+export default MarkdownEditor;

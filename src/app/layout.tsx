430ef98e90767dddc6854d7e8216fc9a65f1154e
jihyeon-Do <ehwlgus22@gmail.com>
2024-06-26 01:27:02 +0900

    remove : delete unused file import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "./custom.css";
-import "./reset.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Head from "next/head";

855bf7345f5894de2d9ede52039c0cadd8d8ede6
jihyeon-Do <ehwlgus22@gmail.com>
2024-07-26 04:57:21 +0900

    fix : IInvalid src prop error resolution

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false, // React Strict Mode 비활성화
+  images: {
+    domains: ["traveltales.s3.ap-northeast-2.amazonaws.com"], // 이곳에 에러에서 hostname 다음 따옴표에 오는 링크를 적으면 된다.
+  },
 };
 
 export default nextConfig;

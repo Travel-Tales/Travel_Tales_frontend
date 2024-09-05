c4903e7069b4827d3f8418841830e5bae591291a
jihyeon-Do <ehwlgus22@gmail.com>
2024-09-05 23:58:57 +0900

    hotfix : build dynamic server error

--- src/app/travel/plans/page.tsx
+++ src/app/travel/plans/page.tsx
@@ -6,27 +6,24 @@ import Category from "@/components/Category";
 import { apiClient } from "@/service/interceptor";
 
 async function getPlans() {
-  try {
-    const headers = {
-      "Content-Type": "application/json",
-    };
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/api/post`,
-      {
-        method: "GET",
-        headers,
-        cache: "no-store",
-      }
-    );
-    if (!response.ok) {
-      throw new Error("Failed to fetch plans.");
-    }
-    const json = await response.json();
-    return { jsonData: json.data, accessToken: "null" };
-  } catch (error) {
-    console.error("API 요청 중 오류 발생:", error);
-    return { jsonData: [], accessToken: "null" };
+  // try {
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/post`, {
+    method: "GET",
+    headers,
+    cache: "no-store",
+  });
+  if (!response.ok) {
+    throw new Error("Failed to fetch plans.");
   }
+  const json = await response.json();
+  return { jsonData: json.data, accessToken: "null" };
+  // } catch (error) {
+  //   console.error("API 요청 중 오류 발생:", error);
+  //   return { jsonData: [], accessToken: "null" };
+  // }
 }
 
 export default async function TravelPlansPage() {

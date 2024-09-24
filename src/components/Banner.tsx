be441465c4a2a00fdcc3b81c367aed78420b50ee
jihyeon-Do <ehwlgus22@gmail.com>
2024-09-24 20:59:12 +0900

    style : change thumbnail image

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,7 +5,7 @@ export default async function Banner() {
   return (
     <section
       className="banner-section text-center bg-main-banner bg-cover 
-bg-bottom md:bg-bottom-1 p-10 sm:p-20 xs:p-7 text-white"
+bg-center md:bg-bottom-1 p-10 sm:p-20 xs:p-7 text-white bg-no-repeat"
     >
       <h2 className="text-h3 sm:text-h2 font-bold">Your Travel Plans</h2>
       <p className="my-5 text-sm sm:text-base">
